3a985c102ffd58b684312446a8445bf8227261e1
Njoga <wooskinner@yahoo.com>
2025-06-29 11:45:27 +0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Scroll Progress Bar
-window.onscroll = function() {
-  var scrollProgress = document.getElementById("scrollProgress");
-  var totalHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrollPosition = document.documentElement.scrollTop;
-  var scrollPercentage = (scrollPosition / totalHeight) * 100;
-  scrollProgress.style.width = scrollPercentage + "%";
-};
